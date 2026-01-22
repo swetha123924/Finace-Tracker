5313edf4a44882a6bf758107aa3b281613683661
swetha123924 <cswetha061@gmail.com>
2026-01-22 12:00:33 +0530

    update frontend url

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,9 @@ const port = process.env.PORT || 5001;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173'],
+  origin: [
+    'https://bucolic-taiyaki-6a6cd5.netlify.app/'
+  ],
   credentials: true
 }));
 app.use(express.json());
